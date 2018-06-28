4f60ab013b484bc537b969737514fecf6abda31a
aragaer <aragaer@gmail.com>
2018-06-28 20:14:33 +0300

    client now accepts args

--- pa-client/app/Main.hs
+++ pa-client/app/Main.hs
@@ -1,13 +1,15 @@
 module Main where
 
 import Network.HaskellNet.IMAP
+import System.Environment
 
 import Lib
 
 main :: IO ()
 main = do
-     conn <- connectIMAPPort "netbook-eth" 8007
-     login conn "niege@brain" "secret"
+     args <- getArgs
+     conn <- connectIMAPPort (args!!0) $ read (args!!1)
+     login conn (args!!2) (args!!3)
      select conn "INBOX"
      outputAll conn
      mainLoop conn
