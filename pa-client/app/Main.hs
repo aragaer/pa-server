ed317ae1113a4a8ee20658f7a920f0157e61bcba
aragaer <aragaer@gmail.com>
2018-07-18 18:22:39 +0300

    Client: line buffering

--- pa-client/app/Main.hs
+++ pa-client/app/Main.hs
@@ -2,11 +2,13 @@ module Main where
 
 import Network.HaskellNet.IMAP
 import System.Environment
+import System.IO
 
 import Lib
 
 main :: IO ()
 main = do
+     hSetBuffering stdout LineBuffering
      args <- getArgs
      conn <- connectIMAPPort (args!!0) $ read (args!!1)
      login conn (args!!2) (args!!3)
--- pa-client/src/Lib.hs
+++ pa-client/src/Lib.hs
@@ -1,33 +1,33 @@
 module Lib
-    ( output
-    , outputAll
+    ( outputAll
     , mainLoop
     ) where
 
-import Data.Text
-import Data.Text.Encoding
+import Control.Monad
+import qualified Data.ByteString.Char8 as BS8
+import Data.String.Utils
 import Network.HaskellNet.IMAP
 import Network.HaskellNet.IMAP.Connection
 import Network.HaskellNet.IMAP.Types
-import System.IO
 
+getText :: [(String, String)] -> [BS8.ByteString]
+getText m = do
+    (f, d) <- m
+    guard (f == "RFC822.TEXT")
+    return $ BS8.pack $ strip d
 
 output :: IMAPConnection -> UID -> IO ()
 output conn uid = do
-       msg <- fetch conn uid
-       putStr $ unpack $ decodeUtf8 msg
-       store conn uid $ PlusFlags [Seen, Deleted]
-       hFlush stdout
+    fetchByString conn uid "(RFC822.TEXT)"
+        >>= BS8.putStr . BS8.unlines . getText
+    store conn uid $ PlusFlags [Seen, Deleted]
 
-outputAll :: IMAPConnection -> IO ()
 outputAll conn = do
-     found <- search conn [ALLs]
-     mapM (output conn) found
-     expunge conn
-     return ()
+    search conn [ALLs] >>= mapM (output conn)
+    expunge conn
 
 mainLoop :: IMAPConnection -> IO ()
 mainLoop conn = do
-     idle conn 10000
-     outputAll conn
-     mainLoop conn
+    idle conn 2000
+    outputAll conn
+    mainLoop conn
